c8c279c9d80e1b1efe828d332665a980d9762e4a
Jovertki <wa9head@gmail.com>
2022-01-28 19:40:15 +0300

    request_handler_added

--- webserv_core/utils/Request.cpp
+++ webserv_core/utils/Request.cpp
@@ -5,12 +5,10 @@
 #include <iostream>
 
 //#define MIME_FILE "../resources/mime.types"
-#define BUFFER_SIZE 30000 //is always bigger then 8000, max HTTP header size
+// #define BUFFER_SIZE 30000 //is always bigger then 8000, max HTTP header size
 //buffer size defined not here
 ft::Request::Request() {
 	clear();
-	set_full_request_length( BUFFER_SIZE );
-	parsing_header = true;
 	parsing_data_header = true;
 	stage = REQUEST_PENDING;
 	
@@ -20,7 +18,16 @@ ft::Request::Request() {
 }
 
 void ft::Request::set_cgi(char** envp) {
-	cgi_handler = CGI_handler( envp, &fd, &requested_url, &query_string, &method, &params );
+	cgi_handler = CGI_handler( envp, &fd, &requested_url, \
+		& query_string, &method, &params );
+}
+
+void ft::Request::set_request_handler() {
+	if(!rhandler.is_initialised()){
+		rhandler = Request_handler( &fd, &method, &requested_url, \
+			& httpver, &params, &query_string );
+	}
+
 }
 // ft::Request::Request( const ft::Request& a ) : method( a.method ), requested_url( a.requested_url ), httpver( a.httpver ), \
 // header_length( a.header_length ), query_string( a.query_string ), params( a.params ), \
@@ -92,24 +99,9 @@ std::string ft::Request::get_content_type() const {
 	return "no such type";
 }
 
-// std::ofstream ft::Request::get_body_fd() const {
-// 	return body_file;
-// }
-
 std::string ft::Request::get_query_string() const {
 	return query_string;
 }
-
-
-long ft::Request::get_total_bytes_read() const {
-	return total_bytes_read;
-}
-
-long ft::Request::get_full_request_length() const {
-	return full_request_length;
-}
-
-
 
 void ft::Request::set_method(const int &n ) {
 	method = n;
@@ -121,17 +113,9 @@ void ft::Request::set_httpver( const std::string& n ) {
 	httpver = n;
 }
 
-// void ft::Request::set_body( const std::ofstream& n ) {
-// 	body_file = n;
-// }
-
-
 void ft::Request::set_query_string( const std::string& n ) {
 	query_string.insert(query_string.begin(), n.begin(), n.end());
 }
-// void ft::Request::set_body_args() {
-// 	query_string = get_body();
-// }
 
 void ft::Request::set_params( const std::map <std::string, std::string>& n ) {
 	params = n;
@@ -165,36 +149,15 @@ std::string ft::Request::get_param_value( const std::string& n ) {
 		return "";
 }
 
-void ft::Request::set_header_length( const int& n) {
-	header_length = n;
-}
-
-int ft::Request::get_header_length()const {
-	return header_length;
-}
-
 void ft::Request::clear() {
 	method = 0;
 	requested_url = "";
 	httpver = "";
-	header_length = 0;
-	// body_file.close();
-	std::remove( BUFFER_FILE );
 	query_string.clear();
 	params.clear();
-	total_bytes_read = 0;
-	full_request_length = 0;
 	cgi_handler = CGI_handler();
 }
 
 void ft::Request::set_param( const std::string& key, const std::string& value ) {
 	params[key] = value;
-}
-
-void ft::Request::set_total_bytes_read(const long& n) {
-	total_bytes_read = n;
-}
-
-void ft::Request::set_full_request_length( const long& n ) {
-	full_request_length = n;
 }
--- webserv_core/utils/Request.hpp
+++ webserv_core/utils/Request.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <fstream>
 #include "CGI_handler.hpp"
+#include "Request_handler.hpp"
 
 namespace ft {
 	class Request {
@@ -15,23 +16,20 @@ namespace ft {
 		std::string httpver;
 		std::string query_string;
 		std::string get_requested_filename() const;
-		
-		int header_length;
-
-		long total_bytes_read;
-		long full_request_length;
 	public:
 		//remain in request
 		int stage;
-		CGI_handler cgi_handler;
 		int fd;
+		CGI_handler cgi_handler;
+		Request_handler rhandler;
 		std::map <std::string, std::string> params;
 
+		//used in sending response
 		unsigned long lastPos;
 
-		
-		bool parsing_header;
+		//remove candidate, used in handle multipart
 		bool parsing_data_header;
+		
 		Request();
 		// Request( const Request& a );
 		// Request& operator=( const Request& a );
@@ -43,28 +41,21 @@ namespace ft {
 		// std::ofstream get_body_fd() const;
 		std::string get_query_string() const;
 		std::string get_param_value( const std::string& n );
-		int get_header_length()const;
-		// std::map<std::string, std::string>::iterator get_params_begin() const;
-		// std::map<std::string, std::string>::iterator get_params_end() const;
-		long get_total_bytes_read() const;
-		long get_full_request_length() const;
 		
-		void set_cgi(char** envp);
+		void set_cgi( char** envp );
+		void set_request_handler();
+
 		void set_method( const int& );
 		void set_requested_url(const std::string&);
 		void set_httpver(const std::string&);
-		// void set_body( const std::ofstream& );
 		void set_query_string( const std::string& );
-		// void set_body_args();
+		
 		void set_params( const std::map <std::string, std::string>& );
 		void set_param( const std::string& key, const std::string& value );
-		void set_header_length( const int& );
-		void set_total_bytes_read( const long& );
-		void set_full_request_length( const long& );
-		
 		void insert_param( const std::pair<std::string, std::string>& );
 		void print_params();
 		int param_exists( const std::string& ) const;
+	
 		void clear();
 	};
 }
