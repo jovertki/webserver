1e25c2e30b1109a1378b95d907db158608f43a1d
Jovertki <wa9head@gmail.com>
2022-02-03 21:53:07 +0300

    deleted_comments

--- webserv_core/servers/WebServer.cpp
+++ webserv_core/servers/WebServer.cpp
@@ -168,48 +168,6 @@ void ft::WebServer::handle_errors(const int& error_code, Request& request ) {
 	request.set_stage( RESPONCE_GENERATED );
 }
 
-// void ft::WebServer::handle_errors( int error_code, Request& request ) {
-// 	std::ostringstream header;
-// 	std::ostringstream body;
-
-// 	body << "<!DOCTYPE html>" << std::endl << \
-// 		"<html lang=\"en\">" << std::endl << std::endl << \
-// 		"<head>" << std::endl << \
-// 		"<title>" << error_code << " Error Page</title>" << std::endl << std::endl << \
-// 		"<meta charset=\"utf-8\">" << std::endl << \
-// 		"<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">" << std::endl << \
-// 		"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">" << std::endl << \
-// 		"<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->" << std::endl << std::endl << \
-// 		"<!-- Bootstrap -->" << std::endl << std::endl << \
-// 		"<link rel = \"stylesheet\" href = \"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\">" << std::endl << std::endl << \
-// 		"<!-- Custom stlylesheet -->" << std::endl << \
-// 		"<link type=\"text/css\" rel=\"stylesheet\" href=\"/css/style.css\" />" << std::endl << std::endl << \
-// 		"</head>" << std::endl << std::endl << \
-// 		"<body>" << std::endl << \
-// 		"<div class=\"vertical-center\">" << std::endl << \
-// 		"<div class=\"container\">" << std::endl << \
-// 		"<div id=\"notfound\" class=\"text-center\">" << std::endl << \
-// 		"<h1>:-(</h1>" << std::endl << \
-// 		"<h1>" << error_code << "</h1>" << std::endl << \
-// 		"<p>" << response_messeges[error_code] << "</p>" << std::endl << \
-// 		"<a href=\"/\">Back to homepage</a>" << std::endl << \
-// 		"</div>" << std::endl << \
-// 		"</div>" << std::endl << \
-// 		"</div>" << std::endl << \
-// 		"</body>" << std::endl << \
-// 		"</html>" << std::endl;
-
-// 	header << request.get_httpver() << " " << error_code << " " << "KO" << std::endl <<//<- needs elaboration
-// 		"Content-Type: text/html;" << std::endl << \
-// 		"Content-Length: " << body.str().size() << std::endl << std::endl;
-// 	std::ofstream response_file;
-// 	response_file.open( BUFFER_FILE_OUT + std::to_string( request.get_fd() ), std::ios::binary );
-// 	response_file << header.str() << body.str();
-// 	response_file.close();
-// 	request.set_stage( RESPONCE_GENERATED );
-
-// 	// throw (error_request_code());
-// }
 void ft::WebServer::list_contents( const std::string& path, Request& request )const {
 	std::ostringstream header;
 	std::ostringstream body;
@@ -229,8 +187,6 @@ void ft::WebServer::list_contents( const std::string& path, Request& request )co
 		"</head>" << std::endl << std::endl << \
 		"<body>" << std::endl << \
 		"<h1>Contents\n\n</h1> " << std::endl;
-
-
 	DIR* dir;
 	struct dirent* ent;
 	if((dir = opendir( path.c_str() )) != NULL) {
